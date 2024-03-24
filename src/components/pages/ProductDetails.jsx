49ce68599d7b61a590b078986f703c5fcd192eb3
Rajesh Korlapati <rajeshkorlapati465@gmail.com>
2024-03-24 16:26:58 +0530

    Add: Product Details Page Added And Styled

--- src/components/pages/ProductDetails.jsx
+++ src/components/pages/ProductDetails.jsx
@@ -1,34 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { useParams,useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
+import "./ProductDetails.css";
+import { FaLessThan } from "react-icons/fa";
 
 const ProductDetails = () => {
   const { id } = useParams();
   const [productDetails, setProductDetails] = useState(null); // Change initial state to null
-  
   useEffect(() => {
     axios
       .get(`https://dummyjson.com/products/${id}`)
       .then((res) => setProductDetails(res.data))
-      .catch((error) => console.error("Error fetching product details:", error)); // Handle errors
-      console.log(productDetails)
+      .catch((error) =>
+        console.error("Error fetching product details:", error)
+      ); // Handle errors
+    console.log(productDetails);
   }, [id]);
-  
- const history = useNavigate()
- const handleHome=()=>{
-    history('/')
- }
-  
+
+  const history = useNavigate();
+  const handleHome = () => {
+    history("/");
+  };
 
   return (
-    <div style={{marginTop:'10%'}}>
-       
-        <img src={productDetails?.images[0]}/>
-        <p>{productDetails?.title}</p>
-       <span><b>${productDetails?.price}</b> <p>{productDetails?.rating}⭐</p></span> 
-       <button className="button">BuyNow</button><br/>
-       <button onClick={()=>handleHome()}>Back To Home</button>
-     
+    <div className="product-details-container">
+      <span
+        style={{ position: "absolute", left: 10, top: 30, cursor: "pointer" }}
+        onClick={() => handleHome()}>
+        <FaLessThan />
+      </span>
+      <div className="product-details-image-cotainer">
+        <img
+          className="product-details-image"
+          height={400}
+          src={productDetails?.images[0]}
+        />
+        <div>
+          <img src={productDetails?.images[1]} width={55} height={50} />
+          <img src={productDetails?.images[2]} width={55} height={50} />
+          <img src={productDetails?.images[3]} width={55} height={50} />
+        </div>
+      </div>
+      <div className="product-details-wraper">
+        <h2 className="product-details-title">{productDetails?.title}</h2>
+        <p className="product-rating">{productDetails?.rating}⭐</p>
+
+        <p className="product-details-description">
+          {productDetails?.description}
+        </p>
+        <span>
+        <p className="product-details-discount">{productDetails?.discountPercentage}% off</p>
+
+        <b className="product-details-price">${productDetails?.price}</b>
+        <s>${productDetails?.price*2}</s>
+        </span>
+
+        <button className="button">BuyNow</button>
+        <br />
+      </div>
     </div>
   );
 };
